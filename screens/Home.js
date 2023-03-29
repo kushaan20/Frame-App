0934779d20778c253f798c448e7273c510ae5c8e
Kushaan Agarwal <64969376+kushaan20@users.noreply.github.com>
2023-03-29 12:17:14 +0530

    Add files via upload

---
+++ screens/Home.js
@@ -0,0 +1,3 @@
+import React,{Component} from 'react';
+import {View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Platform, StatusBar,Image} from 'react-native';
+import {RFValue} from 'react-native-responsive-fontsize';
